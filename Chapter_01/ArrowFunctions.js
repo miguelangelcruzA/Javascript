30e04f7ac90c158cee8508071bd9d361a41494b9
miguel Angel <miguelseid15@gmail.com>
2023-09-16 21:32:52 -0600

    updated
    commit

--- Chapter_01/ArrowFunctions.js
+++ Chapter_01/ArrowFunctions.js
@@ -2,8 +2,43 @@
 
 //Arrow function or fat arrow functions
 
-() => {
-    console.log(test)
+test = () => {
+    let arr = [10,10,23,211,23];
+     arr.filter(item => item == 10).length;
 }
 
-test();
+
+
+// test();
+
+// let arr = [10,10,23,211,23];
+
+
+// let exists = arr.filter(item => item == 10);
+
+// console.log(exists.length)
+
+let srt = "JavaScript";
+
+function reverseString(str) {
+    let splitString = str.split("");
+    let join = "";
+    for (i=splitString.length-1;i>=0;i--){
+         join += splitString[i];
+    }
+ 
+    return join;
+ }
+
+
+ console.log(reverseString(srt));
+
+
+
+
+ function replaceChar(string, char){
+
+    return string.replace(char,"#")
+ }
+
+ console.log(replaceChar("hello world",'l'));
